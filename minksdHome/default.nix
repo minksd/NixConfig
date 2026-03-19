b8aa46296cad52b00107a2247d61b3f0b4913d80
Daniel Minks <danielminks1230@gmail.com>
2026-03-19 14:53:13 -0400

    Moved to latest kernel

--- minksdHome/default.nix
+++ minksdHome/default.nix
@@ -46,7 +46,7 @@ inputs.nixpkgs.lib.nixosSystem rec {
     (
       { config, ... }:
       {
-        boot.kernelPackages = config._module.args.upkgs.linuxPackages_latest; # or specialArgs.upkgs.linuxKernel.packages.linux_x_xx for specific kernel
+        boot.kernelPackages = config._module.args.upkgs.linuxPackages_testing; # or specialArgs.upkgs.linuxKernel.packages.linux_x_xx for specific kernel
 
         #nvidia/graphics
         hardware = {
